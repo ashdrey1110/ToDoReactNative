f8ddbb29ead98fc8ced993493db1368648494c1e
ashdrey1110 <ashrey1110@gmail.com>
2025-06-26 18:57:37 -0700

    modified frontend

--- todo-app/components/EditScreenInfo.tsx
+++ todo-app/components/EditScreenInfo.tsx
@@ -1,46 +1,59 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { ExternalLink } from "./ExternalLink";
+import { MonoText } from "./StyledText";
+import { Text, View } from "./Themed";
+import { useEffect, useState } from "react";
 
-import { ExternalLink } from './ExternalLink';
-import { MonoText } from './StyledText';
-import { Text, View } from './Themed';
-
-import Colors from '@/constants/Colors';
+import Colors from "@/constants/Colors";
 
 export default function EditScreenInfo({ path }: { path: string }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/list`)
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+      })
+      .catch((e) => console.error("Failed to fetch items", e));
+  }, []);
+
   return (
     <View>
       <View style={styles.getStartedContainer}>
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
+          darkColor="rgba(255,255,255,0.8)"
+        >
+          Hello
         </Text>
 
         <View
           style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
           darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)">
+          lightColor="rgba(0,0,0,0.05)"
+        >
           <MonoText>{path}</MonoText>
         </View>
 
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
+          darkColor="rgba(255,255,255,0.8)"
+        >
+          Change any of the text, save the file, and your app will automatically
+          update.
         </Text>
       </View>
 
       <View style={styles.helpContainer}>
-        <ExternalLink
-          style={styles.helpLink}
-          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
-          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
-          </Text>
-        </ExternalLink>
+        <MaterialIcons name="add-circle" size={24} />
+        <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
+          Add new item
+        </Text>
       </View>
     </View>
   );
@@ -48,8 +61,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
 
 const styles = StyleSheet.create({
   getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+    marginHorizontal: 25,
   },
   homeScreenFilename: {
     marginVertical: 7,
@@ -61,17 +73,13 @@ const styles = StyleSheet.create({
   getStartedText: {
     fontSize: 17,
     lineHeight: 24,
-    textAlign: 'center',
   },
   helpContainer: {
     marginTop: 15,
     marginHorizontal: 20,
-    alignItems: 'center',
   },
   helpLink: {
     paddingVertical: 15,
   },
-  helpLinkText: {
-    textAlign: 'center',
-  },
+  helpLinkText: { flex: 1, justifyContent: "center" },
 });
